b896bbd3b49f76ab8b5753ac6d22f4525a902415
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-10-17 14:02:27 +0900

    Create EXP.coded.js

---
+++ EXP.coded.js
@@ -0,0 +1,31 @@
+
+//lex https://codepen.io/gnjo/pen/abZNKOp?editors=0010
+//lexjump 
+function lex(d){
+ function _c(d){return d.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm,'')} 
+
+ let ma=/([A-Z!#$@*><_][0-9A-Z!#$@*><_]{2})\.([\s\S]*?)END|([A-Z!#$@*><_][0-9A-Z!#$@*><_]{2})(?:\n|$)|([A-Z!#$@*><_][0-9A-Z!#$@*><_]{2})[ =](.*)(?:\n|$)/g
+ let ma_dot=/([A-Z!#$@*><_][0-9A-Z!#$@*><_]{2})\.([\s\S]*?)END/
+ let ma_eq=/([A-Z!#$@*><_][0-9A-Z!#$@*><_]{2})=(.*)(?:\n|$)/
+ let ma_mark=/(MRK|###|<<<) (.*)(?:\n|$)/
+ let ma_jump=/(IFJ|>>>) (.*)(?:\n|$)/
+ let a
+ ;
+ return _c(d).match(ma).map(d=>d.trim()).map(d=>{
+  if(ma_dot.test(d))return a=d.match(ma_dot),(a[1]+' '+a[2]).trim()
+  else if(ma_mark.test(d))return a=d.match(ma_mark),('<<<'+' '+a[2]).trim()
+  else if(ma_jump.test(d))return a=d.match(ma_jump),('>>>'+' '+a[2]).trim()  
+  else if(ma_eq.test(d))return 'EVL'+' '+d  
+  else return d
+ })
+}
+function lexjump(ary){
+ let ma_mark=/(MRK|###|<<<) (.*)(?:\n|$)/
+ let obj={}
+ ary.map((d,i)=>{
+  if(!ma_mark.test(d))return d
+  let a=d.match(ma_mark)
+  obj[a[2]]=i
+ })
+ return obj
+}
