a5cf30949fdedc494633fc162243fc765b96e404
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-10-18 20:12:46 +0900

    Create r027.js

---
+++ test/r027.js
@@ -0,0 +1,66 @@
+//r027
+EXP({debug:console.log})
+EXP`640x360
+MIM <<<,MRK
+MIM >>>,IFJ
+MIM ***,WAI
+FON 18px planes 
+VOL 0.1
+COR #fff
+///////////
+CAS.
+https://gnjo.github.io/mock3d/bg/back2.jpg
+https://gnjo.github.io/mock3d/bg/back3.jpg
+
+https://gnjo.github.io/mock3d/se/slash1.ogg
+
+https://gnjo.github.io/mock3d/mons/ajin1-min.png
+https://gnjo.github.io/mock3d/mons/man2-min.png
+END //
+///////////
+<<< #story1
+CLR #000
+IMG back2.jpg,f,0,0
+IMG back2.jpg,f,0,0,#000a //暗く
+IMG man2-min.png,l,0,0,#000c
+CAP story1
+*** 10
+<<< #story1.loop
+DRW story1,700 //fadein 700ms
+MES.
+夜の静けさよりも、何よりも。
+迷宮は暗い。
+しかし、暗闇の中では更なる五感が冴え渡る。
+蠢く者の気配。地上より、よりはっきりと判る。
+ここは、名もなき迷宮。
+
+今あらためて、この街は建て直され始めている。
+この街は、旧王都に旧支配地、封鎖された迷宮。
+第三皇子といっては、既に古い。
+先王は、豊かな資源を元に迷宮を封鎖していた。
+迷宮を王都の中に囲み、きつく厳重する。
+それは、神話や伝承からの慎重さだった。
+
+古きに古いだろうか。
+他国では積極的に迷宮を利用する帰来にある。
+伝承では、魔物の檻たる迷宮から魔物這い出し、
+地上を覆った時代があったという。
+先王はその懸念から迷宮の封鎖を決めたのだった。
+
+しかし。突然起こった破壊の一撃。
+この街の王都は崩れ、今しばらく経つ。
+王都防衛の第一皇子、交易の第二皇子は絶えた。
+遊学の第三皇子は遠方で生き延びる。
+灰と化した王都へ凱旋した第三皇子。
+そうして、この街は建て直され始めたのである。
+
+迷宮の活用に先を見る。
+人の野心を逆手に取るとは、
+卑賤だろうか、辣腕だろうか。
+この街の多くが小さな王宮の庇護下にある――。
+END
+SE_ slash1.ogg
+DRW #000,700 //fadeoutの場合は#000に向かって行う。
+SE_ 
+>>> #story1.loop
+`
